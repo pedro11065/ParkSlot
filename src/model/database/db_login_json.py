9a00034df0b5eba1942a0bbe2b6242b5bf03415c
Pedro Quix <83504805+pedro11065@users.noreply.github.com>
2024-08-16 23:38:29 -0300

    16/18 end

--- src/model/database/db_login_json.py
+++ src/model/database/db_login_json.py
@@ -4,9 +4,11 @@ import json
 #sys.path.insert(1, 'parkslot/src/view')
 #from view.db_login import login_register
 
+file_path = "c:/Users/fsxre/OneDrive/GitHub/ParkSlot/src/model/database/login.json"
+
 sys.path.insert(1, 'parkslot/src/model/database')
 
-with open("login.json","r") as file: #abrindo .json
+with open(file_path,"r") as file: #abrindo .json
     data = json.load(file)
 
-print(data)
+print(data['last_login']['logged']) #descobrindo valor de logged
